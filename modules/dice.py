08980c83b1a3a585560aea7257daf36865a99ffb
MaxUNof <maxunof@gmail.com>
2020-05-14 14:50:03 +0300

    modules: dice.py

---
+++ modules/dice.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Coded by @maxunof with power of Senko! 
+
+import moduling
+import utils
+from telethon.tl.types import InputMediaDice
+
+class Module(moduling.Module):
+    def __init__(self):
+        self.name = "Dice"
+
+    async def dicecmd(self, db, client, message, cmd):
+        if len(cmd.args) == 0:
+           await utils.send(message, "<b>Incorrect command usage!</b>\n<b>You can pass two arguments: number and emoji.</b>")
+           return
+        need = 0
+        try:
+            need = int(cmd.args[0])
+        except:
+            await utils.send(message, "<b>You need to specify the number that should fall out.</b>")
+            return
+        emoji = ""
+        if len(cmd.args) > 1:
+            emoji = cmd.args[1]
+        await message.delete()
+        count = 0
+        while count != 15:
+            msg = await client.send_file(message.chat_id, InputMediaDice(emoji))
+            if msg.media.value == need:
+                return
+            await msg.delete()
+            count = count+1
