d80711a6fec6f107eae80abad4b5b469d10418e7
Maxim <maxunof@gmail.com>
2021-01-05 23:47:50 +0300

    cmd.py: ???

--- cmd.py
+++ cmd.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Coded by @maxunof with power of Senko!
 
+import re
 
 class Command():
     def __init__(self, text):
@@ -10,7 +11,7 @@ class Command():
         self.args = []
         if text.startswith("."):
             self.full = text[1:]
-            split = self.full.split(" ", 1)
+            split = re.split(r'\s', self.full, 1)
             self.cmd = split[0]
             if len(split) > 1:
                 self.arg = split[1]
